bac56999be036b6a29568262c8a6516c5befd6e4
mdshahco <mdshahcoding.gmail.com>
2023-09-24 23:36:04 +0600

    final

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".menu > ul > li").click(function (e) {
+ //remove active from already active
+    // $(this).siblings().removeClass("active");
+    //add active to clicked
+    $(this).toggleClass("active")
+    //if has sub menu open it
+    $(this).find("ul").slideToggle();
+    //close other sub menu if any one
+    $(this).siblings().find("ul").find("li").removeClass("active");
+});
+
